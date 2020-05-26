497d22c596ab04524c7d2cdc4679a0b667482ca4
loxygenk <loxygen.k@gmail.com>
2020-05-26 12:37:04 +0900

    †追加† Lintが動いた

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { TestComp } from "comps/TestComp"
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+				{TestComp}
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
