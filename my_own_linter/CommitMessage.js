254e4ec87a11cd222fbb86ae16a3a7c51599c24b
loxygenk <loxygen.k@gmail.com>
2020-05-27 19:00:53 +0900

    †追加† 色は必要

--- my_own_linter/CommitMessage.js
+++ my_own_linter/CommitMessage.js
@@ -1,9 +1,15 @@
 const fs = require("fs");
 
+const consoleRed = "\u001b[31m";
+const consoleGreen = "\u001b[32m";
+const consoleBold = "\u001b[1m";
+const consoleThin = "\u001b[2m";
+const consoleColorReset = "\u001b[0m";
+
 const prefix = ["追加", "削除", "編集", "修正", "競合"];
 const gitMessage = fs.readFileSync("./.git/COMMIT_EDITMSG", "utf8").trim();
 
-console.log(`"${gitMessage}"を確認しています…`);
+console.log(`${consoleThin}"${gitMessage}"を確認しています…${consoleColorReset}`);
 
 const doesMatched = prefix.some((str) => {
   const pattern = new RegExp(String.raw`^†${str}† .{1,60}`, "i");
@@ -11,10 +17,10 @@ const doesMatched = prefix.some((str) => {
 });
 
 if (!doesMatched) {
-  console.log("†キレた†");
-  console.log("コミットメッセージの形式が合っていません");
+  console.log(`${consoleRed}${consoleBold}†キレた†${consoleColorReset}`);
+  console.log(`コミットメッセージの形式が合っていません`);
 } else {
-  console.log("†Approved†");
+  console.log(`${consoleGreen}${consoleBold}†Approved†${consoleColorReset}`);
 }
 
 process.exit(!doesMatched);
