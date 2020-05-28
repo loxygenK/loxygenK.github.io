fd5c186857643bb03106fbff4f65c0c2f048260a
loxygenk <loxygen.k@gmail.com>
2020-05-28 13:03:55 +0900

    †修正† おう！テストしてからコミットしろカス！

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {TestComp}
+				<TestComp />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
