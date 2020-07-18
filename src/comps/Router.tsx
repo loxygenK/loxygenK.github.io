a95e8e670b7f31f384850735e27a6c190a5c4230
loxygenK <loxygen.k@gmail.com>
2020-07-18 20:47:25 +0900

    †修正† そろそろ動いてクレメンス

--- src/comps/Router.tsx
+++ src/comps/Router.tsx
@@ -1,17 +1,17 @@
 import * as React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { Switch, Route, HashRouter } from "react-router-dom";
 import { HomePage } from "./pages/home/HomePage";
 import { NotFoundPage } from "./pages/Error404";
 import { WhoAmIPage } from "./pages/who/WhoAmIPage";
 
 export function AppRouter(): JSX.Element {
   return (
-    <BrowserRouter basename="/">
+    <HashRouter basename="/">
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/who" component={WhoAmIPage} />
         <Route path="" component={NotFoundPage} />
       </Switch>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
