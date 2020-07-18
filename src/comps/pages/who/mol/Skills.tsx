de9c99e8519205089bfa3151b379655d0e4167fb
loxygenK <loxygen.k@gmail.com>
2020-07-18 17:03:28 +0900

    †追加† コンポーネントを分けて、複数列描画できるようにした

--- src/comps/pages/who/mol/Skills.tsx
+++ src/comps/pages/who/mol/Skills.tsx
@@ -1,8 +1,8 @@
 import * as React from "react";
 import styled from "styled-components";
 import { FrameworkSkills, LangSkills } from "../../../../data/LangSkills";
-import { SkillListElement } from "../atom/SkilListElement";
 import { SubTitle } from "../../../common/Formatter";
+import { SkillList } from "./SkillList";
 
 const SkillWrapper = styled.div`
   display: block;
@@ -10,33 +10,12 @@ const SkillWrapper = styled.div`
   margin-bottom: 3em;
 `;
 
-const ContentsHeader = styled.div`
-  font-size: 1.2em;
-  margin-bottom: 0.75em;
-`;
-
 export function Skills() {
   return (
     <SkillWrapper>
       <SubTitle>使えるもの</SubTitle>
-      <ContentsHeader>言語</ContentsHeader>
-      {LangSkills.map((skill, index) => (
-        <SkillListElement
-          name={skill.name}
-          description={skill.description}
-          level={skill.level}
-          key={index}
-        />
-      ))}
-      <ContentsHeader>フレームワーク</ContentsHeader>
-      {FrameworkSkills.map((skill, index) => (
-        <SkillListElement
-          name={skill.name}
-          description={skill.description}
-          level={skill.level}
-          key={index}
-        />
-      ))}
+      <SkillList caption={"言語"} skills={LangSkills} />
+      <SkillList caption={"フレームワーク"} skills={FrameworkSkills} />
     </SkillWrapper>
   );
 }
