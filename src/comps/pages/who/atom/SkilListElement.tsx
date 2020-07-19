8331bf1bd6974a0a84cfc4f7a1fd6f07d768b192
loxygenK <loxygen.k@gmail.com>
2020-07-19 18:44:21 +0900

    †編集† Skillのリストの名前に下線を付けた

--- src/comps/pages/who/atom/SkilListElement.tsx
+++ src/comps/pages/who/atom/SkilListElement.tsx
@@ -4,17 +4,22 @@ import { BigText, SubText } from "../../../common/Formatter";
 import { LevelComponent } from "../../../common/types/LevelComponent";
 import styled from "styled-components";
 
-export const SkillListElementRoot = styled.div`
+const SkillListElementRoot = styled.div`
   padding: 1em;
   margin-bottom: 0.8em;
 
   background-color: #2a2a4b;
 `;
 
+const SkillTitle = styled(BigText)`
+  display: block;
+  border-bottom: 3px solid #494974;
+`;
+
 export function SkillListElement(skill: SkillData) {
   return (
     <SkillListElementRoot>
-      <BigText>{skill.name}</BigText>
+      <SkillTitle>{skill.name}</SkillTitle>
       <LevelComponent level={skill.level} />
       <SubText>{skill.description}</SubText>
     </SkillListElementRoot>
