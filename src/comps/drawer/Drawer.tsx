496ed928dcf6dd63127b5ad1a1e3b2678694f527
flisan <55672846+loxygenK@users.noreply.github.com>
2020-07-19 14:30:45 +0900

    ドロワーを閉じる動作を改善した (#19)
    * †修正† ドロワーの後ろに背景がついた
    
    * †修正† ドロワーの後ろがぼけるようになった
    
    * †修正† ドロワーの後ろをタップするとドロワーが引っ込むようになった
    
    * †削除† 不要になったものを消した

--- src/comps/drawer/Drawer.tsx
+++ src/comps/drawer/Drawer.tsx
@@ -4,6 +4,7 @@ import { useSpring, animated } from "react-spring";
 import { Logo } from "../common/Logo";
 import { PageDestination } from "../../data/types/PageDestination";
 import { DrawerNavigation } from "./mol/DrawerNavigation";
+import { ModalBackground } from "./atom/ModalBackground";
 
 const DrawerRoot = styled.div`
   background-color: #292942;
@@ -24,17 +25,6 @@ const DrawerRoot = styled.div`
   height: 100vh;
 `;
 
-const CloseButton = styled.div`
-  margin: 0.9em 1em;
-  padding: 0.2em 0;
-
-  border-top: 1px solid #43436c;
-
-  font-size: 0.75em;
-
-  user-select: none;
-`;
-
 const AnimatedDrawerRoot = animated(DrawerRoot);
 
 type DrawerProps = {
@@ -45,18 +35,20 @@ type DrawerProps = {
 
 export function Drawer(props: DrawerProps) {
   const { x } = useSpring({
-    x: props.isOpen ? 0 : -15,
+    x: props.isOpen ? 0 : -10,
   });
 
   return (
-    <AnimatedDrawerRoot
-      style={{
-        left: x.interpolate((o) => `${o}em`),
-      }}
-    >
-      <Logo />
-      <DrawerNavigation pages={props.pages} />
-      <CloseButton onClick={props.onClosed}>閉じる</CloseButton>
-    </AnimatedDrawerRoot>
+    <div>
+      <ModalBackground enabled={props.isOpen} onClick={props.onClosed} />
+      <AnimatedDrawerRoot
+        style={{
+          left: x.interpolate((o) => `${o}em`),
+        }}
+      >
+        <Logo />
+        <DrawerNavigation pages={props.pages} />
+      </AnimatedDrawerRoot>
+    </div>
   );
 }
---
+++ src/comps/drawer/atom/ModalBackground.tsx
@@ -0,0 +1,34 @@
+import * as React from "react";
+import styled from "styled-components";
+import { animated, useTransition } from "react-spring";
+
+const ModalBackgroundRoot = styled.div`
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+
+  backdrop-filter: blur(10px);
+`;
+
+const AnimatedModalBackgroundRoot = animated(ModalBackgroundRoot);
+
+export function ModalBackground(props: {
+  enabled: boolean;
+  onClick: () => void;
+}) {
+  const transitions = useTransition(props.enabled, null, {
+    from: { opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 },
+  });
+  return (
+    <div onClick={props.onClick}>
+      {transitions.map(
+        ({ item, key, props }) =>
+          item && <AnimatedModalBackgroundRoot key={key} style={props} />
+      )}
+    </div>
+  );
+}
