328d02a75080428fda8b2dc56cbf22c55b7d92d7
loxygenK <loxygenK@users.noreply.github.com>
2020-07-19 05:32:48 +0000

    deploy: 496ed928dcf6dd63127b5ad1a1e3b2678694f527

---
+++ precache-manifest.eee912180dc66d4f15da51127fa4ba05.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ea33de2a42a56c15892325a4d21e0f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1fcedc0e6a3f224bf13a",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "cf8e154ba9c00d1ac6c1",
+    "url": "/static/js/2.e6d76327.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e6d76327.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fcedc0e6a3f224bf13a",
+    "url": "/static/js/main.d614b4a7.chunk.js"
+  },
+  {
+    "revision": "dd88ee6d06e8c3caff2f",
+    "url": "/static/js/runtime-main.6f0fee2b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8616333a74fa895d3957112f3c1015dd.js"
+  "/precache-manifest.eee912180dc66d4f15da51127fa4ba05.js"
 );
 
 self.addEventListener('message', (event) => {
